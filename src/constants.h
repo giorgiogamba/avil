ff03b227ffa6b75af0ef7ef098ae59d9b80b9320
Giorgio Gamba <giorgiogamba97@gmail.com>
2026-02-14 17:56:56 +0100

    cleanup code and apply homogeneous coding conventions

--- src/constants.h
+++ src/constants.h
@@ -9,3 +9,9 @@ constexpr int DISPLAY_SIZE{25};
 // Defines spectrogram's boundaries
 constexpr int SPECTROGRAM_FREQ_START{20};
 constexpr int SPECTROGRAM_FREQ_END{20 * 1000};
+
+constexpr float GAIN_REDUCTION_FACTOR{0.1f};
+constexpr double MIN_ENERGY{1e-6};
+constexpr double MIN_RATIO{0.005};
+constexpr int SECONDS_TO_SKIP{5};
+constexpr const int FRAMES_TO_CHECK{100};
